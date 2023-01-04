48c3f234e960b54580b339555dbcd21f770620f3
Giancarlo Naranjo <72670316+hackerbot1901@users.noreply.github.com>
2023-01-04 00:33:32 -0500

    Creacion de la clase Colegio.kt, modificacion de la clase Conexion.kt con la creacion de metodos crear, actualizar, borrar de la tabla colegio

---
+++ ExamenKotlin/app/src/main/java/com/example/examenkotlin/Colegio.kt
@@ -0,0 +1,10 @@
+package com.example.examenkotlin
+
+class Colegio {
+    val fechaCreacion: String? = ""
+    // String --> Char
+    val bloque: String = ""
+    val inversion = 0.0
+    val nombre = ""
+    val idColegio = 0
+}
--- ExamenKotlin/app/src/main/java/com/example/examenkotlin/Conexion.kt
+++ ExamenKotlin/app/src/main/java/com/example/examenkotlin/Conexion.kt
@@ -18,21 +18,56 @@ class Conexion(context: Context) :
         private const val ESTATURA = "estatura"
         private const val MATRICULA = "matricula"
         private const val ECONOMIA = "economia"
+
+        //Crear las variables para la base de datos colegio
+        private const val TBL_COLEGIO = "tbl_colegio"
+        private const val IDCOLEGIO = "id" //Int
+        private const val NOMBRECOLEGIO = "nombre_colegio" //String
+        private const val INVERSION = "inversion_colegio" //Double
+        private const val BLOQUE = "bloque" //Char
+        private const val FECHACREACION = "fecha_creacion" //Date
+
+        //Foreing key en la tabla alumno de colegio
+        private const val FKCOLEGIO =
+            "FOREIGN KEY($IDCOLEGIO) REFERENCES $TBL_ALUMNO($IDALUMNO)"
     }
 
     override fun onCreate(db: SQLiteDatabase?) {
-        val createTableAlumno = ("CREATE TABLE " + TBL_ALUMNO + "("
-                + IDALUMNO + " INTEGER PRIMARY KEY," + NOMBRE + " TEXT," +
-                ESTATURA + " real," + MATRICULA + " INTEGER," + ECONOMIA + " real" +
-                ")")
+        //Creacion de la tabla colegio
+        val createTableColegio = (
+                "CREATE TABLE " + TBL_COLEGIO +
+                        "(" +
+                        IDCOLEGIO + " INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL," +
+                        NOMBRECOLEGIO + " TEXT," +
+                        INVERSION + " real," +
+                        BLOQUE + " TEXT," +
+                        FECHACREACION + " TEXT," +
+                        ")"
+                )
+        db?.execSQL(createTableColegio)
+
+        val createTableAlumno = (
+                "CREATE TABLE " + TBL_ALUMNO +
+                        "(" +
+                        IDALUMNO + " INTEGER PRIMARY KEY," +
+                        NOMBRE + " TEXT," +
+                        ESTATURA + " real," +
+                        MATRICULA + " INTEGER," +
+                        ECONOMIA + " real" +
+                        FKCOLEGIO +
+                        ")"
+                )
         db?.execSQL(createTableAlumno)
+
+
     }
 
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
-        db!!.execSQL("DROP TABLE IF EXISTS $TBL_ALUMNO")
+        db!!.execSQL("DROP TABLE IF EXISTS $TBL_ALUMNO;\n")
         onCreate(db)
     }
 
+    //Metodos CRUD para alumnos
     fun crearAlumno(alumno: Alumno): Long {
         val db = this.writableDatabase
         val cv = ContentValues()
@@ -105,9 +140,42 @@ class Conexion(context: Context) :
         val success = db.delete(TBL_ALUMNO, "id=$idAlumno", null)
         db.close()
         return success
-
-
     }
 
+    //Metodos CRUD para colegio
+    fun crearColegio(colegio: Colegio): Long {
+        val db = this.writableDatabase
+        val cv = ContentValues()
+        cv.put(IDCOLEGIO, colegio.idColegio)
+        cv.put(NOMBRECOLEGIO, colegio.nombre)
+        cv.put(INVERSION, colegio.inversion)
+        cv.put(BLOQUE, colegio.bloque)
+        cv.put(FECHACREACION, colegio.fechaCreacion)
 
+        val success = db.insert(TBL_COLEGIO, null, cv)
+        db.close()
+        return success
+    }
+
+    fun eliminarColegio(idColegio: Int): Int {
+        val db = this.writableDatabase
+        val cv = ContentValues()
+        cv.put(IDCOLEGIO, idColegio)
+        val success = db.delete(TBL_COLEGIO, "id=$idColegio", null)
+        db.close()
+        return success
+    }
+
+    fun actualizarColegio(colegio: Colegio): Int {
+        val db = this.writableDatabase
+        val cv = ContentValues()
+        cv.put(IDCOLEGIO, colegio.idColegio)
+        cv.put(NOMBRECOLEGIO, colegio.nombre)
+        cv.put(INVERSION, colegio.inversion)
+        cv.put(BLOQUE, colegio.bloque)
+        cv.put(FECHACREACION, colegio.fechaCreacion)
+        val success = db.update(TBL_COLEGIO, cv, "id=" + colegio.idColegio, null)
+        db.close()
+        return success
+    }
 }
